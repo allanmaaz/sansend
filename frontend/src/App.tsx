26cccdbac22576b99b90371fcfe951151124c424
allanmaaz <allanmaazimdad@gmail.com>
2026-03-22 00:13:55 +0530

    Fix glitchy background by replacing hardcoded inline styles with CSS animated orbs

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -6,9 +6,9 @@ function App() {
     return (
         <div className="relative min-h-screen overflow-hidden">
             {/* Background orbs */}
-            <div className="bg-orb" style={{ width: 600, height: 600, top: '-10%', left: '-10%', background: '#6366f1' }} />
-            <div className="bg-orb" style={{ width: 400, height: 400, bottom: '-5%', right: '-5%', background: '#8b5cf6' }} />
-            <div className="bg-orb" style={{ width: 300, height: 300, top: '50%', left: '50%', transform: 'translate(-50%, -50%)', background: '#4f46e5' }} />
+            <div className="bg-orb bg-orb-1"></div>
+            <div className="bg-orb bg-orb-2"></div>
+            <div className="bg-orb bg-orb-3"></div>
 
             {/* Content */}
             <div className="relative z-10">
